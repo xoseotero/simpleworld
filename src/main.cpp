1b3c97e03430434112515192ab851644893e3693
Xosé Otero <xoseotero@gmail.com>
2007-01-23 12:16:48 +0000

    Add the Simple World main file
    It's only a "Hello World!" right now.

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file src/main.cpp
+ * Simple World
+ *
+ * begin:     
+ * last:      $Date$ by $Author$
+ *
+ *  Copyright (C) 2006, Xosé Otero <xoseotero@users.sourceforge.net>
+ *
+ *  License: See COPYING file that comes with this distribution
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Hello, world!" << std::endl;
+
+  std::exit(EXIT_SUCCESS);
+}
