e29751505a017ac534ddd5ab6b37c9c09d248958
Xosé Otero <xoseotero@gmail.com>
2007-01-23 12:24:12 +0000

    Add cmake infrastructure to the project

---
+++ simple/config.h.cmake
@@ -0,0 +1,15 @@
+#ifndef SIMPLEWORLDCONFIG_H
+#define SIMPLEWORLDCONFIG_H
+
+#define EXIT_SUCCESS 0
+
+#cmakedefine IS_BIG_ENDIAN
+#cmakedefine IS_LITTLE_ENDIAN
+
+#cmakedefine UNIX
+#cmakedefine WIN32
+
+#cmakedefine HAVE_LONG_LONG
+#cmakedefine HAVE__INT64
+
+#endif // SIMPLEWORLDCONFIG_H
