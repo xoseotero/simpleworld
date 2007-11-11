ab37620587c15c9e9d8b860650996fa80ffb3b5d
Xosé Otero <xoseotero@gmail.com>
2007-11-11 22:49:47 +0000

    Add Element class, the top class of the elements on Simple World

---
+++ simpleworld/element.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file simpleworld/element.cpp
+ * A element on the World
+ *
+ * begin:     Thu, 19 Jul 2007 08:47:15 +0200
+ * last:      $Date$
+ *
+ *  Copyright (C) 2007  Xosé Antón Otero Ferreira <xoseotero@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "element.hpp"
+
+namespace SimpleWorld
+{
+
+Element::Element(ElementType type, Position position)
+  : type(type), position(position)
+{
+  if (type == ElementBug)
+    this->movable_ = true;
+  else
+    this->movable_ = false;
+}
+
+}
---
+++ simpleworld/element.hpp
@@ -0,0 +1,75 @@
+/**
+ * @file simpleworld/element.h
+ * A element on the World.
+ *
+ * begin:     Mon, 05 Mar 2007 09:11:19 +0100
+ * last:      $Date$ by $Author$
+ *
+ *  Copyright (C) 2007  Xosé Otero <xoseotero@users.sourceforge.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SIMPLEWORLD_ELEMENT_HPP__
+#define __SIMPLEWORLD_ELEMENT_HPP__
+
+#include <simpleworld/types.hpp>
+
+namespace SimpleWorld
+{
+
+/**
+ * A element on the World.
+ */
+class Element
+{
+public:
+  /**
+   * Constructor.
+   * @param type type of element
+   * @param position position of the element.
+   */
+  Element(ElementType type, Position position);
+
+  /**
+   * Destructor.
+   */
+  virtual ~Element() {}
+
+
+  /**
+   * Type of element.
+   */
+  ElementType type;
+
+
+  /**
+   * Position of the element.
+   */
+  Position position;
+
+
+  /**
+   * Check if the element can be moved.
+   * @return if the element can be moved.
+   */
+  bool movable() const { return this->movable_; }
+
+private:
+  bool movable_;
+};
+
+}
+
+#endif // __SIMPLEWORLD_ELEMENT_HPP__
