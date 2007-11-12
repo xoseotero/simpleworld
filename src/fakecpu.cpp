e35c7937b655ae35e6ab2098224e0ba43386a9d5
Xosé Otero <xoseotero@gmail.com>
2007-11-12 02:57:51 +0000

    Add FakeCPU that add a "world" operation that does nothing

---
+++ src/fakecpu.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file src/fakecpu.cpp
+ * Fake CPU that add a "world" operation that does nothing.
+ *
+ * begin:     Sun, 04 Nov 2007 20:26:14 +0100
+ * last:      $Date$
+ *
+ *  Copyright (C) 2007  Xosé Antón Otero Ferreira <xoseotero@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "fakecpu.hpp"
+
+#include <simpleworld/types.hpp>
+#include <cpu/memory.hpp>
+#include <cpu/interrupt.hpp>
+#include <cpu/instruction.hpp>
+
+#define REGISTER(NUMBER) (NUMBER * sizeof(cpu::Word))
+#define REGISTER_PC REGISTER(0xd)
+#define REGISTER_STP REGISTER(0xe)
+#define REGISTER_ITP REGISTER(0xf)
+
+/**
+ * Fake World operation that does nothing.
+ * @param regs the registers.
+ * @param mem the memory.
+ * @param interrupt interrupt.
+ * @param inst the instruction.
+ * @return if the PC must be updated.
+ */
+cpu::Update fakeworld(cpu::Memory& regs, cpu::Memory& mem,
+                      cpu::Interrupt& interrupt, cpu::Instruction inst)
+{
+  // The operation can be done always.
+  // The actual values of the registers are used to return the false values of
+  // the operation.
+  regs.set_word(REGISTER(0), static_cast<cpu::Word>(sw::ActionOk));
+
+  return cpu::UpdatePC;
+}
+
+
+FakeCPU::FakeCPU(cpu::Memory* registers, cpu::Memory* memory)
+  : cpu::CPU(registers, memory)
+{
+  this->set_.add_instruction(0x38, "world", 0, true, fakeworld);
+}
---
+++ src/fakecpu.hpp
@@ -0,0 +1,46 @@
+/**
+ * @file src/fakecpu.hpp
+ * Fake CPU that add a "world" operation that does nothing.
+ *
+ * begin:     Sun, 04 Nov 2007 20:26:14 +0100
+ * last:      $Date$
+ *
+ *  Copyright (C) 2007  Xosé Antón Otero Ferreira <xoseotero@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __FAKECPU_HPP__
+#define __FAKECPU_HPP__
+
+#include <cpu/cpu.hpp>
+namespace sw = SimpleWorld;
+namespace cpu = SimpleWorld::CPU;
+
+/**
+ * Fake CPU that add a "world" operation that does nothing.
+ */
+class FakeCPU: public cpu::CPU
+{
+public:
+  /**
+   * Constructor.
+   * The registers size can change to allow at least 16 registers.
+   * @param registers registers of the CPU.
+   * @param memory memory of the CPU.
+   */
+  FakeCPU(cpu::Memory* registers, cpu::Memory* memory);
+};
+
+#endif // __FAKECPU_HPP__
