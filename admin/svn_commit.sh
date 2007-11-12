dd2baa5d834f4ff788376b1f86c06794c8f0dfbf
Xosé Otero <xoseotero@gmail.com>
2007-11-12 04:53:20 +0000

    Add svn_commit.sh

---
+++ admin/svn_commit.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+# Add diffstat information to the commits.
+
+SVN=svn
+DIFFSTAT=diffstat
+TEMPFILE=commit.txt
+EDITOR=nano
+RM=rm
+
+echo "comment for the commit here" > ${TEMPFILE}
+echo >> ${TEMPFILE}
+echo " -------------------" >> ${TEMPFILE}
+
+${SVN} diff $* | ${DIFFSTAT} >> ${TEMPFILE}
+${EDITOR} ${TEMPFILE}
+${SVN} ci -N -F ${TEMPFILE} $*
+${RM} ${TEMPFILE}
