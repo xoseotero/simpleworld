c1b455c6d6de97c5a9627c48bf3e074754939b59
Xosé Otero <xoseotero@gmail.com>
2007-11-28 05:45:46 +0000

    Divide the unit tests of old InstructionSet
    Divide the unit tests into:
     1. unit test of Instruction
     2. unit test of ISA

--- tests/instruction_test.cpp
+++ tests/instruction_test.cpp
@@ -21,9 +21,9 @@ BOOST_AUTO_TEST_CASE(instruction_encode)
   inst.second = 0x4;
   inst.address = 0x5678;
 #ifdef IS_BIG_ENDIAN
-  BOOST_CHECK_EQUAL(cpu::InstructionSet::encode(inst), 0x12345678);
+  BOOST_CHECK_EQUAL(cpu::Instruction::encode(inst), 0x12345678);
 #else
-  BOOST_CHECK_EQUAL(cpu::InstructionSet::encode(inst), 0x78563412);
+  BOOST_CHECK_EQUAL(cpu::Instruction::encode(inst), 0x78563412);
 #endif
 }
 
@@ -36,57 +36,10 @@ BOOST_AUTO_TEST_CASE(instruction_decode)
 #else
   sw::Uint32 inst_coded = 0x78563412;
 #endif
-  cpu::Instruction inst = cpu::InstructionSet::decode(inst_coded);
+  cpu::Instruction inst = cpu::Instruction::decode(inst_coded);
 
   BOOST_CHECK_EQUAL(inst.code, 0x12);
   BOOST_CHECK_EQUAL(inst.first, 0x3);
   BOOST_CHECK_EQUAL(inst.second, 0x4);
   BOOST_CHECK_EQUAL(inst.address, 0x5678);
-}
-
-BOOST_AUTO_TEST_CASE(instruction_add_register)
-{
-  BOOST_CHECKPOINT("Add a register");
-
-  cpu::InstructionSet set;
-
-  sw::Uint32 code = 0xfd;
-  std::string name = "test";
-
-  set.add_register(code, name);
-
-  BOOST_CHECK_EQUAL(set.register_code(name), code);
-  BOOST_CHECK_EQUAL(set.register_name(code), name);
-  std::vector<sw::Uint8> registers = set.register_codes();
-  BOOST_CHECK(find(registers.begin(), registers.end(), code) !=
-              registers.end());
-
-  set.remove_register(code);
-}
-
-BOOST_AUTO_TEST_CASE(instruction_add_instruction)
-{
-  BOOST_CHECKPOINT("Add a instruction");
-
-  cpu::InstructionSet set;
-  cpu::InstructionInfo info;
-  info.code = 0xfd;
-  info.name = "test";
-  info.nregs = 2;
-  info.has_inmediate = true;
-
-  set.add_instruction(info);
-
-  BOOST_CHECK_EQUAL(set.instruction_code(info.name), info.code);
-  BOOST_CHECK_EQUAL(set.instruction_info(info.code).code, info.code);
-  BOOST_CHECK_EQUAL(set.instruction_info(info.code).name, info.name);
-  BOOST_CHECK_EQUAL(set.instruction_info(info.code).nregs, info.nregs);
-  BOOST_CHECK_EQUAL(set.instruction_info(info.code).has_inmediate,
-                    info.has_inmediate);
-  BOOST_CHECK_EQUAL(set.instruction_info(info.code).func, info.func);
-  std::vector<sw::Uint8> instructions = set.instruction_codes();
-  BOOST_CHECK(find(instructions.begin(), instructions.end(), info.code) !=
-              instructions.end());
-
-  set.remove_instruction(info.code);
 }
---
+++ tests/isa_test.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+
+#define BOOST_TEST_MODULE Unit test for isa.hpp
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <simpleworld/config.hpp>
+#include <simpleworld/ints.hpp>
+#include <simpleworld/cpu/isa.hpp>
+namespace sw = SimpleWorld;
+namespace cpu = SimpleWorld::CPU;
+
+
+BOOST_AUTO_TEST_CASE(instruction_add_register)
+{
+  BOOST_CHECKPOINT("Add a register");
+
+  cpu::ISA isa;
+
+  sw::Uint32 code = 0xfd;
+  std::string name = "test";
+
+  isa.add_register(code, name);
+
+  BOOST_CHECK_EQUAL(isa.register_code(name), code);
+  BOOST_CHECK_EQUAL(isa.register_name(code), name);
+  std::vector<sw::Uint8> registers = isa.register_codes();
+  BOOST_CHECK(find(registers.begin(), registers.end(), code) !=
+              registers.end());
+
+  isa.remove_register(code);
+}
+
+BOOST_AUTO_TEST_CASE(instruction_add_interrupt)
+{
+  BOOST_CHECKPOINT("Add a interrupt");
+
+  cpu::ISA isa;
+
+  sw::Uint32 code = 0xfd;
+  std::string name = "test";
+
+  isa.add_interrupt(code, name);
+
+  BOOST_CHECK_EQUAL(isa.interrupt_code(name), code);
+  BOOST_CHECK_EQUAL(isa.interrupt_name(code), name);
+  std::vector<sw::Uint8> interrupts = isa.interrupt_codes();
+  BOOST_CHECK(find(interrupts.begin(), interrupts.end(), code) !=
+              interrupts.end());
+
+  isa.remove_interrupt(code);
+}
+
+BOOST_AUTO_TEST_CASE(instruction_add_instruction)
+{
+  BOOST_CHECKPOINT("Add a instruction");
+
+  cpu::ISA isa;
+  cpu::InstructionInfo info;
+  info.code = 0xfd;
+  info.name = "test";
+  info.nregs = 2;
+  info.has_inmediate = true;
+
+  isa.add_instruction(info);
+
+  BOOST_CHECK_EQUAL(isa.instruction_code(info.name), info.code);
+  BOOST_CHECK_EQUAL(isa.instruction_info(info.code).code, info.code);
+  BOOST_CHECK_EQUAL(isa.instruction_info(info.code).name, info.name);
+  BOOST_CHECK_EQUAL(isa.instruction_info(info.code).nregs, info.nregs);
+  BOOST_CHECK_EQUAL(isa.instruction_info(info.code).has_inmediate,
+                    info.has_inmediate);
+  BOOST_CHECK_EQUAL(isa.instruction_info(info.code).func, info.func);
+  std::vector<sw::Uint8> instructions = isa.instruction_codes();
+  BOOST_CHECK(find(instructions.begin(), instructions.end(), info.code) !=
+              instructions.end());
+
+  isa.remove_instruction(info.code);
+}
