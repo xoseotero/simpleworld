02ac84012b971fbc5f013787d7402720363abfca
Xosé Otero <xoseotero@gmail.com>
2007-11-30 18:57:31 +0000

    Add headers to the unit tests

--- tests/cpu/word_test.cpp
+++ tests/cpu/word_test.cpp
@@ -1,3 +1,26 @@
+/**
+ * @file tests/cpu/word_test.cpp
+ * Unit test for cpu/word.hpp.
+ *
+ * begin:     Fri, 30 Nov 2007 18:19:17 +0100
+ * last:      $Date$
+ *
+ *  Copyright (C) 2007  Xosé Otero <xoseotero@users.sourceforge.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define BOOST_TEST_MODULE Unit test for cpu/word.hpp
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
