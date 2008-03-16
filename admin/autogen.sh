0ef18c16a990f680ed8e31c5338e1cbefcaa2f68
Xosé Otero <xoseotero@gmail.com>
2008-03-16 15:03:04 +0000

    Add options to create debug and release builds

--- admin/autogen.sh
+++ admin/autogen.sh
@@ -1,12 +1,96 @@
 #! /bin/sh
 # Prepare the project to be build in the directory build/.
-# The prefix path can be passed as argument optionally
+#
+# Use: autogen.sh [OPTIONS] PREFIX
+#   PREFIX                     directory where to install
+#   --release                  create a release build
+#   --debug                    create a debug build
 
-mkdir build
-cd build
+release=0
+debug=0
+prefix=""
+directory="build/"
+options="-D RUN_TESTS=ON"
 
-if [ "$1" != "" ]; then
-	cmake -D CMAKE_INSTALL_PREFIX=$1 ..
-else
-	cmake ..
+
+usage()
+{
+    echo "\
+Use: autogen.sh [OPTIONS] PREFIX
+Prepare the project to be build in the directory build/ with PREFIX as
+the install destination.
+
+Options:
+  --release                  create a release build
+  --debug                    create debug information
+
+  --help                     show this text"
+
+    exit $1
+}
+
+parse_cmd()
+{
+    if [ "$1" == "--help" ]; then
+	usage 0
+    elif [ "$1" == "--release" ]; then
+	release=1
+	shift
+	parse_cmd $*
+    elif [ "$1" == "--debug" ]; then
+	debug=1
+	shift
+	parse_cmd $*
+    elif [ "$1" != "" ]; then
+	prefix=$1
+    fi
+
+    if [ $# -gt 1 ]; then
+	echo "Too many or wrong parameters."
+	usage 1
+    fi
+}
+
+create_build()
+{
+    directory=$1
+    options=$2
+
+    rm -rf ${directory}
+    mkdir ${directory}
+    cd ${directory}
+
+    cmake ${options} ..
+
+    if [ "${prefix}" != "" ]; then
+	ln -s ${prefix} install_dir
+    fi
+
+    cd ..
+}
+
+
+# parse command line and set the options
+parse_cmd $*
+if [ "${prefix}" != "" ]; then
+    options="${options} -D CMAKE_INSTALL_PREFIX=${prefix}"
 fi
+if [ "${debug}" == 1 ]; then
+    options="${options} \
+      -D CMAKE_BUILD_TYPE=Debug \
+      -D DEBUG_MODE=ON \
+      -D RUN_TESTS=ON \
+      -D SQLite3_USE_INTERNAL=ON \
+      -D SQLite3x_USE_INTERNAL=ON \
+      -D XySSL_USE_INTERNAL=ON \
+      -D getopt_USE_INTERNAL=ON"
+fi
+if [ "${release}" == 1 ]; then
+    options="${options} \
+      -D CMAKE_BUILD_TYPE=Release"
+fi
+
+echo "create_build ${directory} ${options}"
+create_build "${directory}" "${options}"
+
+exit 0
