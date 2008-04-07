fa08c069b600017ca31b9aedc21778e7f3cab149
Xosé Otero <xoseotero@gmail.com>
2008-04-07 19:46:41 +0000

    Add first unit test for db

---
+++ tests/db/db_test.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file tests/db/db_test.cpp
+ * Unit test for DB::DB.
+ *
+ * begin:     Tue, 04 Dec 2007 05:33:07 +0100
+ * last:      $Date$
+ *
+ *  Copyright (C) 2007  Xosé Otero <xoseotero@users.sourceforge.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define BOOST_TEST_MODULE Unit test for db::DB
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <simpleworld/ints.hpp>
+#include <simpleworld/db/types.hpp>
+#include <simpleworld/db/db.hpp>
+namespace sw = simpleworld;
+namespace db = simpleworld::db;
+
+
+#define DB_FILE (TESTDATA "db.sw")
+#define DB_SAVE (TESTOUTPUT "temp.sw")
+
+
+/**
+ * Get the version.
+ */
+BOOST_AUTO_TEST_CASE(db_version)
+{
+  db::DB sw(DB_SAVE);
+  sw::Uint8 version = sw.version();
+
+  BOOST_CHECK_EQUAL(version, db::DB(DB_SAVE).version());
+}
