7f146d9ff5f12b552a44084e79689f42a86de9e2
Xosé Otero <xoseotero@gmail.com>
2009-11-24 10:15:27 +0100

    Delete subversion scripts

--- admin/svn_props.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# Set the SVN properties for source files.
-
-for file in $*; do
-	svn propset svn:mime-type text/plain ${file}
-	svn propset svn:eol-style native ${file}
-	svn propset svn:keywords "Date Author Id Revision HeadURL" ${file}
-done
