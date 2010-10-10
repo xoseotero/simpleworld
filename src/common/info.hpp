bc862e555e924d72ce2b28fdd6735a519326a2cb
Xosé Otero <xoseotero@gmail.com>
2010-10-11 01:32:15 +0200

    Simple World 1.0b2 (now with all files included!)
    Oops! File forgoten!

---
+++ src/common/info.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file src/common/info.hpp
+ * Information.
+ *
+ *  Copyright (C) 2010  Xosé Otero <xoseotero@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+#define VERSION "1.0b2"
+#define YEARS "2006-2010"
+#define AUTHOR "Xosé Otero"
+#define EMAIL "xoseotero@gmail.com"
+#define MAILBUGS "simpleworld-list@lists.sourceforge.net"
+
+#endif // CONFIG_HPP
