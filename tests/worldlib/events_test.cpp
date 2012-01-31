d83809e7b9ba199e6f7ca18362927d652aa4d886
Xosé Otero <xoseotero@gmail.com>
2012-02-01 00:13:22 +0100

    worldlib: Events management

---
+++ tests/worldlib/events_test.cpp
@@ -0,0 +1,350 @@
+/**
+ * @file tests/worldlib/events_test.cpp
+ * Unit test for worldlib/events.swl
+ *
+ *  Copyright (C) 2012  Xosé Otero <xoseotero@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define BOOST_TEST_MODULE Unit test for worldlib/events.swl
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <simpleworld/cpu/memory.hpp>
+#include <simpleworld/cpu/memory_file.hpp>
+#include <simpleworld/cpu/file.hpp>
+#include <simpleworld/cpu/source.hpp>
+namespace sw = simpleworld;
+namespace cpu = simpleworld::cpu;
+
+#include "src/common/fakecpu.hpp"
+
+
+#define REGISTER(cpu, name) ADDRESS((cpu).isa().register_code(name))
+#define CPU_SAVE (TESTOUTPUT "events.swo")
+#define MAX_CYCLES 6144
+
+
+/**
+ * Compile a file to CPU_SAVE.
+ * @param file file to compile
+ */
+void compile(const cpu::File& file)
+{
+  cpu::Memory registers;
+  FakeCPU cpu(&registers, NULL);
+
+  std::vector<std::string> include_path;
+  include_path.push_back(INCLUDE_DIR);
+  cpu::Source source(cpu.isa(), include_path);
+
+  source.insert(0, file);
+  source.compile(CPU_SAVE);
+}
+
+
+/**
+ * Compile the file.
+ */
+BOOST_AUTO_TEST_CASE(swl_compile)
+{
+  cpu::File source;
+
+  source.insert(".include \"worldlib/events.swl\"");
+
+  source.insert("world_connect");
+  source.insert("world_disconnect");
+  source.insert("world_events");
+  source.insert("world_throw");
+
+  BOOST_CHECK_NO_THROW(compile(source));
+}
+
+/**
+ * Check WORLD_INIT.
+ */
+BOOST_AUTO_TEST_CASE(world_init)
+{
+  cpu::File source;
+
+  // Initialize
+  source.insert(".label init");
+  source.insert("loada sp stack");
+  source.insert("move fp sp");
+  source.insert("loada g0 heap");
+  source.insert("loadi g1 0x400");
+  source.insert("call std_init");
+  source.insert("loadi g0 0x10");
+  source.insert("call world_init");
+  source.insert("b main");
+
+  source.insert(".include \"stdlib/init.swl\"");
+  source.insert(".include \"worldlib/init.swl\"");
+  source.insert(".include \"worldlib/def.swl\"");
+  source.insert(".include \"worldlib/events/connect.swl\"");
+  source.insert(".include \"worldlib/events/loop.swl\"");
+
+  // Test
+  source.insert(".label main");
+  source.insert("loadi g0 WORLD_INIT");
+  source.insert("loada g1 handler");
+  source.insert("call world_connect");
+
+  source.insert("call world_loop");
+
+  // Event handler
+  source.insert(".label handler");
+  source.insert("loadi g0 0xDABC");
+  source.insert("stop");
+
+  // Space for 256 words in the heap
+  source.insert(".label heap");
+  source.insert(".block 0x400");
+
+  // Space for 32 words in the stack
+  source.insert(".label stack");
+  source.insert(".block 0x80");
+
+  compile(source);
+
+  cpu::Memory registers;
+  cpu::MemoryFile memory(CPU_SAVE);
+  FakeCPU cpu(&registers, &memory);
+  cpu.execute(MAX_CYCLES);
+
+  BOOST_CHECK(not cpu.running());
+  BOOST_CHECK_EQUAL(registers[REGISTER(cpu, "g0")], 0xDABC);
+}
+
+/**
+ * Check WORLD_IDLE.
+ */
+BOOST_AUTO_TEST_CASE(world_idle)
+{
+  cpu::File source;
+
+  // Initialize
+  source.insert(".label init");
+  source.insert("loada sp stack");
+  source.insert("move fp sp");
+  source.insert("loada g0 heap");
+  source.insert("loadi g1 0x400");
+  source.insert("call std_init");
+  source.insert("loadi g0 0x10");
+  source.insert("call world_init");
+  source.insert("b main");
+
+  source.insert(".include \"stdlib/init.swl\"");
+  source.insert(".include \"worldlib/init.swl\"");
+  source.insert(".include \"worldlib/def.swl\"");
+  source.insert(".include \"worldlib/events/connect.swl\"");
+  source.insert(".include \"worldlib/events/loop.swl\"");
+
+  // Test
+  source.insert(".label main");
+  source.insert("loadi g0 WORLD_IDLE");
+  source.insert("loada g1 handler");
+  source.insert("call world_connect");
+
+  source.insert("call world_loop");
+
+  // Event handler
+  source.insert(".label handler");
+  source.insert("loadi g0 0xDABC");
+  source.insert("stop");
+
+  // Space for 256 words in the heap
+  source.insert(".label heap");
+  source.insert(".block 0x400");
+
+  // Space for 32 words in the stack
+  source.insert(".label stack");
+  source.insert(".block 0x80");
+
+  compile(source);
+
+  cpu::Memory registers;
+  cpu::MemoryFile memory(CPU_SAVE);
+  FakeCPU cpu(&registers, &memory);
+  cpu.execute(MAX_CYCLES);
+
+  BOOST_CHECK(not cpu.running());
+  BOOST_CHECK_EQUAL(registers[REGISTER(cpu, "g0")], 0xDABC);
+}
+
+/**
+ * Check world_throw.
+ */
+BOOST_AUTO_TEST_CASE(world_throw)
+{
+  cpu::File source;
+
+  // Initialize
+  source.insert(".label init");
+  source.insert("loada sp stack");
+  source.insert("move fp sp");
+  source.insert("loada g0 heap");
+  source.insert("loadi g1 0x400");
+  source.insert("call std_init");
+  source.insert("loadi g0 0x10");
+  source.insert("call world_init");
+  source.insert("b main");
+
+  source.insert(".include \"stdlib/init.swl\"");
+  source.insert(".include \"stdlib/def.swl\"");
+  source.insert(".include \"worldlib/init.swl\"");
+  source.insert(".include \"worldlib/def.swl\"");
+  source.insert(".include \"worldlib/events/connect.swl\"");
+  source.insert(".include \"worldlib/events/loop.swl\"");
+  source.insert(".include \"worldlib/events/throw.swl\"");
+
+  // Test
+  source.insert(".label main");
+  source.insert("loadi g0 0x911");
+  source.insert("loada g1 handler1");
+  source.insert("call world_connect");
+
+  source.insert("loadi g0 0x911");
+  source.insert("loada g1 handler2");
+  source.insert("call world_connect");
+
+  source.insert("loadi g0 WORLD_IDLE");
+  source.insert("loada g1 handlerstop");
+  source.insert("call world_connect");
+
+  source.insert("loadi g0 0x911");
+  source.insert("call world_throw");
+
+  source.insert("call world_loop");
+
+  // Event handlers
+  source.insert(".label handler1");
+  source.insert("loadi g0 0xDABC");
+  source.insert("store g0 data1");
+  source.insert("ret");
+
+  source.insert(".label handler2");
+  source.insert("loadi g0 0xABCD");
+  source.insert("store g0 data2");
+  source.insert("ret");
+
+  source.insert(".label handlerstop");
+  source.insert("load g0 data1");
+  source.insert("load g1 data2");
+  source.insert("stop");
+
+  // Data
+  source.insert(".label data1");
+  source.insert(".block STD_WORDSIZE");
+  source.insert(".label data2");
+  source.insert(".block STD_WORDSIZE");
+
+  // Space for 256 words in the heap
+  source.insert(".label heap");
+  source.insert(".block 0x400");
+
+  // Space for 32 words in the stack
+  source.insert(".label stack");
+  source.insert(".block 0x80");
+
+  compile(source);
+
+  cpu::Memory registers;
+  cpu::MemoryFile memory(CPU_SAVE);
+  FakeCPU cpu(&registers, &memory);
+  cpu.execute(MAX_CYCLES);
+
+  BOOST_CHECK(not cpu.running());
+  BOOST_CHECK_EQUAL(registers[REGISTER(cpu, "g0")], 0xDABC);
+  BOOST_CHECK_EQUAL(registers[REGISTER(cpu, "g1")], 0xABCD);
+}
+
+/**
+ * Check world_disconnect.
+ */
+BOOST_AUTO_TEST_CASE(world_disconnect)
+{
+  cpu::File source;
+  
+  // Initialize
+  source.insert(".label init");
+  source.insert("loada sp stack");
+  source.insert("move fp sp");
+  source.insert("loada g0 heap");
+  source.insert("loadi g1 0x400");
+  source.insert("call std_init");
+  source.insert("loadi g0 0x10");
+  source.insert("call world_init");
+  source.insert("b main");
+  
+  source.insert(".include \"stdlib/init.swl\"");
+  source.insert(".include \"worldlib/init.swl\"");
+  source.insert(".include \"worldlib/def.swl\"");
+  source.insert(".include \"worldlib/events/connect.swl\"");
+  source.insert(".include \"worldlib/events/disconnect.swl\"");
+  source.insert(".include \"worldlib/events/loop.swl\"");
+  source.insert(".include \"worldlib/events/throw.swl\"");
+  
+  // Test
+  source.insert(".label main");
+  source.insert("loadi g0 0x911");
+  source.insert("loada g1 handler");
+  source.insert("call world_connect");
+
+  source.insert("loadi g0 WORLD_IDLE");
+  source.insert("loada g1 handlerstop");
+  source.insert("call world_connect");
+
+  source.insert("loadi g0 0x911");
+  source.insert("loada g1 handler");
+  source.insert("call world_disconnect");
+
+  source.insert("loadi g0 0x911");
+  source.insert("call world_throw");
+  
+  source.insert("call world_loop");
+  
+  // Event handlers
+  source.insert(".label handler");
+  source.insert("loadi g0 0xDABC");
+  source.insert("store g0 data");
+  source.insert("stop");
+
+  source.insert(".label handlerstop");
+  source.insert("load g0 data");
+  source.insert("stop");
+
+  // Data
+  source.insert(".label data");
+  source.insert(".block STD_WORDSIZE");
+
+  // Space for 256 words in the heap
+  source.insert(".label heap");
+  source.insert(".block 0x400");
+  
+  // Space for 32 words in the stack
+  source.insert(".label stack");
+  source.insert(".block 0x80");
+  
+  compile(source);
+  
+  cpu::Memory registers;
+  cpu::MemoryFile memory(CPU_SAVE);
+  FakeCPU cpu(&registers, &memory);
+  cpu.execute(MAX_CYCLES);
+  
+  BOOST_CHECK(not cpu.running());
+  BOOST_CHECK_NE(registers[REGISTER(cpu, "g0")], 0xDABC);
+}
