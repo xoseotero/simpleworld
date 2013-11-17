3122970045fb3bb26835c81f53e5e2e427e84ad3
Xosé Otero <xoseotero@gmail.com>
2013-11-17 11:28:29 +0100

    db: Add the SQL needed to create the database for the unit tests

---
+++ tests/db/db.sw.sql
@@ -0,0 +1,625 @@
+PRAGMA foreign_keys=OFF;
+PRAGMA user_version=6;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE Environment
+(
+  id INTEGER NOT NULL,
+
+  time INTEGER NOT NULL,
+
+  size_x INTEGER NOT NULL,
+  size_y INTEGER NOT NULL,
+
+  time_rot INTEGER NOT NULL,
+  size_rot INTEGER NOT NULL,
+
+  mutations_probability REAL NOT NULL,
+  time_birth INTEGER NOT NULL,
+  time_mutate INTEGER NOT NULL,
+
+  time_laziness INTEGER NOT NULL,
+  energy_laziness INTEGER NOT NULL,
+
+  attack_multiplier REAL NOT NULL,
+
+  time_nothing INTEGER NOT NULL,
+  time_myself INTEGER NOT NULL,
+  time_detect INTEGER NOT NULL,
+  time_info INTEGER NOT NULL,
+  time_move INTEGER NOT NULL,
+  time_turn INTEGER NOT NULL,
+  time_attack INTEGER NOT NULL,
+  time_eat INTEGER NOT NULL,
+  time_egg INTEGER NOT NULL,
+  energy_nothing INTEGER NOT NULL,
+  energy_myself INTEGER NOT NULL,
+  energy_detect INTEGER NOT NULL,
+  energy_info INTEGER NOT NULL,
+  energy_move INTEGER NOT NULL,
+  energy_turn INTEGER NOT NULL,
+  energy_attack INTEGER NOT NULL,
+  energy_eat INTEGER NOT NULL,
+  energy_egg INTEGER NOT NULL,
+
+  PRIMARY KEY(id),
+  CHECK(time >= 0),
+  CHECK(size_x > 0 AND size_y > 0),
+  CHECK(time_rot >= 0),
+  CHECK(size_rot >= 0),
+  CHECK(mutations_probability >= 0 AND mutations_probability <= 1),
+  CHECK(time_birth >= 0),
+  CHECK(time_mutate >= 0),
+  CHECK(time_laziness >= 0),
+  CHECK(energy_laziness >= 0),
+  CHECK(time_nothing >= 0 AND time_myself >= 0 AND
+        time_detect >= 0 AND time_info >= 0 AND
+        time_move >= 0 AND time_turn >= 0 AND
+        time_attack >= 0 AND time_eat >= 0 AND
+        time_egg >= 0),
+  CHECK(energy_nothing >= 0 AND energy_myself >= 0 AND
+        energy_detect >= 0 AND energy_info >= 0 AND
+        energy_move >= 0 AND energy_turn >= 0 AND
+        energy_attack >= 0 AND energy_eat >= 0 AND
+        energy_egg >= 0)
+);
+
+CREATE TABLE World
+(
+  id INTEGER NOT NULL,
+
+  position_x INTEGER NOT NULL,
+  position_y INTEGER NOT NULL,
+  orientation INTEGER,
+
+  PRIMARY KEY(id),
+  UNIQUE(position_x, position_y),
+  CHECK(position_x >= 0 AND position_y >= 0),
+  CHECK(orientation IS NULL OR
+        (orientation >= 0 AND orientation <= 3))
+);
+
+CREATE TABLE Code(
+  id INTEGER NOT NULL,
+
+  data BLOB NOT NULL,
+
+  PRIMARY KEY(id),
+  CHECK(length(data) > 0 AND (length(data) % 4 = 0))
+);
+
+CREATE TABLE Spawn(
+  id INTEGER NOT NULL,
+
+  code_id INTEGER NOT NULL,
+
+  frequency INTEGER NOT NULL,
+
+  max INTEGER NOT NULL,
+  start_x INTEGER NOT NULL,
+  start_y INTEGER NOT NULL,
+  end_x INTEGER NOT NULL,
+  end_y INTEGER NOT NULL,
+
+  energy INTENER NOT NULL,
+
+  PRIMARY KEY(id),
+  FOREIGN KEY(code_id) REFERENCES Code(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  CHECK(frequency > 0),
+  CHECK(start_x >= 0),
+  CHECK(start_y >= 0),
+  CHECK(end_x > start_x),
+  CHECK(end_y > start_y),
+  CHECK(max <= (end_x - start_x) * (end_y - start_y)),
+  CHECK(energy > 0)
+);
+
+CREATE TABLE Resource(
+  id INTEGER NOT NULL,
+
+  frequency INTEGER NOT NULL,
+
+  max INTEGER NOT NULL,
+  start_x INTEGER NOT NULL,
+  start_y INTEGER NOT NULL,
+  end_x INTEGER NOT NULL,
+  end_y INTEGER NOT NULL,
+
+  size INTENER NOT NULL,
+
+  PRIMARY KEY(id)
+  CHECK(frequency > 0),
+  CHECK(start_x >= 0),
+  CHECK(start_y >= 0),
+  CHECK(end_x > start_x),
+  CHECK(end_y > start_y),
+  CHECK(max <= (end_x - start_x) * (end_y - start_y)),
+  CHECK(size > 0)
+);
+
+CREATE TABLE Bug
+(
+  id INTEGER NOT NULL,
+
+  code_id INTEGER NOT NULL,
+
+  creation INTEGER NOT NULL,
+  father_id INTEGER,
+
+  PRIMARY KEY(id),
+  FOREIGN KEY(code_id) REFERENCES Code(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(father_id) REFERENCES Bug(id) ON UPDATE CASCADE ON DELETE SET NULL,
+  CHECK(creation >= 0)
+);
+
+CREATE TABLE Egg
+(
+  bug_id INTEGER NOT NULL,
+
+  world_id INTEGER NOT NULL,
+  energy INTEGER NOT NULL,
+
+  memory_id INTEGER NOT NULL,
+
+  PRIMARY KEY(bug_id),
+  FOREIGN KEY(bug_id) REFERENCES Bug(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(world_id) REFERENCES World(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(memory_id) REFERENCES Code(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  UNIQUE(world_id),
+  UNIQUE(memory_id),
+  CHECK(energy > 0)
+);
+
+CREATE TABLE Registers(
+  id INTEGER NOT NULL,
+
+  data BLOB NOT NULL,
+
+  PRIMARY KEY(id),
+  CHECK(length(data) = 544)
+);
+
+CREATE TABLE AliveBug
+(
+  bug_id INTEGER NOT NULL,
+
+  world_id INTEGER NOT NULL,
+  birth INTEGER NOT NULL,
+  energy INTEGER NOT NULL,
+
+  time_last_action INTEGER,
+  action_time INTEGER,
+
+  registers_id INTEGER NOT NULL,
+  memory_id INTEGER NOT NULL,
+
+  PRIMARY KEY(bug_id),
+  FOREIGN KEY(bug_id) REFERENCES Bug(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(world_id) REFERENCES World(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(registers_id) REFERENCES Registers(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(memory_id) REFERENCES Code(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  UNIQUE(world_id),
+  UNIQUE(registers_id),
+  UNIQUE(memory_id),
+  CHECK(time_last_action IS NULL OR time_last_action >= 0),
+  CHECK(action_time IS NULL OR action_time > 0),
+  CHECK(birth >= 0),
+  CHECK(energy > 0)
+);
+
+CREATE TABLE DeadBug
+(
+  bug_id INTEGER NOT NULL,
+
+  birth INTEGER,
+  death INTEGER NOT NULL,
+
+  killer_id INTEGER,
+
+  PRIMARY KEY(bug_id),
+  FOREIGN KEY(bug_id) REFERENCES Bug(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY(killer_id) REFERENCES Bug(id) ON UPDATE CASCADE ON DELETE SET NULL,
+  CHECK(birth IS NULL OR birth >= 0),
+  CHECK(birth IS NULL OR birth <= death),
+  CHECK(bug_id <> killer_id)
+);
+
+CREATE TABLE Mutation
+(
+  id INTEGER NOT NULL,
+
+  bug_id INTEGER NOT NULL,
+
+  time INTEGER NOT NULL,
+
+  type INTEGER NOT NULL,
+  position INTEGER NOT NULL,
+  original INTEGER,
+  mutated INTEGER,
+
+  PRIMARY KEY(id),
+  FOREIGN KEY(bug_id) REFERENCES Bug(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  CHECK(time >= 0),
+  CHECK(position >= 0),
+  CHECK(type >= 0 AND type <= 5),
+  CHECK((type == 0 AND original IS NOT NULL AND mutated IS NOT NULL) OR
+        (type == 1 AND original IS NOT NULL AND mutated IS NOT NULL) OR
+        (type == 2 AND original IS NOT NULL AND mutated IS NOT NULL) OR
+        (type == 3 AND original IS NULL AND mutated IS NOT NULL) OR
+        (type == 4 AND original IS NULL AND mutated IS NOT NULL) OR
+        (type == 5 AND original IS NOT NULL AND mutated IS NULL))
+);
+
+CREATE TABLE Food
+(
+  id INTEGER NOT NULL,
+
+  time INTEGER NOT NULL,
+  world_id INTEGER NOT NULL,
+  size INTEGER NOT NULL,
+
+  PRIMARY KEY(id),
+  FOREIGN KEY(world_id) REFERENCES World(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  UNIQUE(world_id),
+  CHECK(time >= 0),
+  CHECK(size >= 0)
+);
+
+CREATE TABLE Stats
+(
+  id INTEGER NOT NULL,
+
+  time INTEGER NOT NULL,
+
+  families INTEGER NO NULL,
+  alive INTEGER NOT NULL,
+  eggs INTEGER NOT NULL,
+  food INTEGER NOT NULL,
+  energy INTEGER NOT NULL,
+  mutations INTEGER NOT NULL,
+  age INTEGER NOT NULL,
+
+  last_births INTEGER NOT NULL,
+  last_sons INTEGER NOT NULL,
+  last_deaths INTEGER NOT NULL,
+  last_kills INTEGER NOT NULL,
+  last_mutations INTEGER NOT NULL,
+
+  PRIMARY KEY(id),
+  CHECK(time >= 0),
+  CHECK(families >= 0),
+  CHECK(alive >= 0),
+  CHECK(eggs >= 0),
+  CHECK(food >= 0),
+  CHECK(energy >= 0),
+  CHECK(mutations >= 0),
+  CHECK(age >= 0),
+  CHECK(last_births >= 0),
+  CHECK(last_sons >= 0),
+  CHECK(last_deaths >= 0),
+  CHECK(last_kills >= 0),
+  CHECK(last_mutations >= 0)
+);
+
+INSERT INTO "Environment" VALUES(1,200,16,16,1024,16,0.001,32,16384,1024,16,2.5,1,2,2,2,3,3,4,4,4,0,1,1,1,2,2,3,3,4);
+INSERT INTO "World" VALUES(1,2,3,0);
+INSERT INTO "World" VALUES(2,11,11,2);
+INSERT INTO "World" VALUES(3,1,7,NULL);
+INSERT INTO "Code" VALUES(1,X'ABABABABABABABAB');
+INSERT INTO "Code" VALUES(2,X'BABABABABABABABA');
+INSERT INTO "Code" VALUES(3,X'1234567890ABCDEF');
+INSERT INTO "Code" VALUES(4,X'1A2B3C4D5E6F7890');
+INSERT INTO "Spawn" VALUES(1,1,1024,12,1,1,5,6,512);
+INSERT INTO "Spawn" VALUES(2,2,4096,16,4,5,8,9,1024);
+INSERT INTO "Resource" VALUES(1,512,10,0,0,16,16,64);
+INSERT INTO "Bug" VALUES(1,1,1,NULL);
+INSERT INTO "Bug" VALUES(2,2,101,NULL);
+INSERT INTO "Bug" VALUES(3,4,200,1);
+INSERT INTO "Egg" VALUES(3,1,100,4);
+INSERT INTO "Registers" VALUES(1,X'00000000111111112222222233333333444444445555555566666666777777770000000000000001000000020000000300000004000000050000000600000007000000000000000100000002000000030000000400000005000000060000000700000000000000010000000200000003000000040000000500000006000000070000000000000001000000020000000300000004000000050000000600000007000000000000000100000002000000030000000400000005000000060000000700000000000000010000000200000003000000040000000500000006000000070000000000000001000000020000000300000004000000050000000600000007000000000000000100000002000000030000000400000005000000060000000700000000000000010000000200000003000000040000000500000006000000070000000000000001000000020000000300000004000000050000000600000007000000000000000100000002000000030000000400000005000000060000000700000000000000010000000200000003000000040000000500000006000000070000000000000001000000020000000300000004000000050000000600000007000000000000000100000002000000030000000400000005000000060000000700000000000000010000000200000003000000040000000500000006000000070000000000000001000000020000000300000004000000050000000600000007');
+INSERT INTO "AliveBug" VALUES(1,2,75,126,189,NULL,1,3);
+INSERT INTO "DeadBug" VALUES(2,100,150,1);
+INSERT INTO "Mutation" VALUES(1,3,50,0,0,1,2);
+INSERT INTO "Mutation" VALUES(2,3,100,1,4,1,1025);
+INSERT INTO "Mutation" VALUES(3,3,200,2,8,1,256);
+INSERT INTO "Mutation" VALUES(4,3,250,3,12,NULL,4);
+INSERT INTO "Mutation" VALUES(5,3,300,4,16,NULL,4);
+INSERT INTO "Mutation" VALUES(6,3,350,5,20,3,NULL);
+INSERT INTO "Food" VALUES(1,2,3,10);
+INSERT INTO "Stats" VALUES(1,1024,90,100,40,32,10000,5,10240,100,10,105,99,20);
+
+CREATE INDEX Environment_index ON Environment(time);
+CREATE INDEX Bug_index ON Bug(father_id);
+CREATE INDEX DeadBug_index ON DeadBug(killer_id);
+CREATE INDEX Mutation_index ON Mutation(bug_id);
+
+CREATE TRIGGER Environment_insert_time
+BEFORE INSERT
+ON Environment
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'There is a older time')
+  WHERE (SELECT max(time)
+         FROM Environment) > NEW.time;
+END;
+
+CREATE TRIGGER Environment_update_time
+BEFORE UPDATE OF time
+ON Environment
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'There is a older time')
+  WHERE (SELECT max(time)
+         FROM Environment) > NEW.time;
+END;
+
+CREATE TRIGGER Environment_insert
+BEFORE INSERT
+ON Environment
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'The size of the World can''t change')
+  WHERE (SELECT count(id)
+         FROM Environment) > 0
+        AND
+        (SELECT id
+         FROM Environment
+         WHERE size_x=NEW.size_x AND size_y=NEW.size_y) IS NULL;
+END;
+
+CREATE TRIGGER World_insert
+BEFORE INSERT
+ON World
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'position_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.position_x;
+  SELECT RAISE(ROLLBACK, 'position_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.position_y;
+END;
+
+CREATE TRIGGER World_update_position_x
+BEFORE UPDATE OF position_x
+ON World
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'position_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.position_x;
+END;
+
+CREATE TRIGGER World_update_position_y
+BEFORE UPDATE OF position_y
+ON World
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'position_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.position_y;
+END;
+
+CREATE TRIGGER Spawn_insert
+BEFORE INSERT
+ON Spawn
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'start_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.start_x;
+  SELECT RAISE(ROLLBACK, 'start_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.start_y;
+  SELECT RAISE(ROLLBACK, 'end_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) < NEW.end_x;
+  SELECT RAISE(ROLLBACK, 'end_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) < NEW.end_y;
+END;
+
+CREATE TRIGGER Spawn_update_start_x
+BEFORE UPDATE OF start_x
+ON Spawn
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'start_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.start_x;
+END;
+
+CREATE TRIGGER Spawn_update_start_y
+BEFORE UPDATE OF start_y
+ON Spawn
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'start_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.start_y;
+END;
+
+CREATE TRIGGER Spawn_update_end_x
+BEFORE UPDATE OF end_x
+ON Spawn
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'end_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) < NEW.end_x;
+END;
+
+CREATE TRIGGER Spawn_update_end_y
+BEFORE UPDATE OF end_y
+ON Spawn
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'end_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) < NEW.end_y;
+END;
+
+CREATE TRIGGER Resource_insert
+BEFORE INSERT
+ON Resource
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'start_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.start_x;
+  SELECT RAISE(ROLLBACK, 'start_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.start_y;
+  SELECT RAISE(ROLLBACK, 'end_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) < NEW.end_x;
+  SELECT RAISE(ROLLBACK, 'end_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) < NEW.end_y;
+END;
+
+CREATE TRIGGER Resource_update_start_x
+BEFORE UPDATE OF start_x
+ON Resource
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'start_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.start_x;
+END;
+
+CREATE TRIGGER Resource_update_start_y
+BEFORE UPDATE OF start_y
+ON Resource
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'start_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) <= NEW.start_y;
+END;
+
+CREATE TRIGGER Resource_update_end_x
+BEFORE UPDATE OF end_x
+ON Resource
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'end_x is out of the World')
+  WHERE (SELECT size_x
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) < NEW.end_x;
+END;
+
+CREATE TRIGGER Resource_update_end_y
+BEFORE UPDATE OF end_y
+ON Resource
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'end_y is out of the World')
+  WHERE (SELECT size_y
+         FROM Environment
+         WHERE time=(SELECT max(time)
+                     FROM Environment)) < NEW.end_y;
+END;
+
+CREATE TRIGGER Bug_insert_creation
+BEFORE INSERT
+ON Bug
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'creation is not now')
+  WHERE (SELECT max(time)
+         FROM Environment) <> NEW.creation;
+END;
+
+CREATE TRIGGER AliveBug_insert_time_last_action
+BEFORE INSERT
+ON AliveBug
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'The time_last_action is in the future')
+  WHERE (SELECT max(time)
+         FROM Environment)
+        < NEW.time_last_action;
+END;
+
+CREATE TRIGGER AliveBug_update_time_last_action
+BEFORE UPDATE OF time_last_action
+ON AliveBug
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'The time_last_action is in the future')
+  WHERE (SELECT max(time)
+         FROM Environment)
+        < NEW.time_last_action;
+END;
+
+CREATE TRIGGER AliveBug_insert_action_time
+BEFORE INSERT
+ON AliveBug
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'The action_time is in the past')
+  WHERE (SELECT max(time)
+         FROM Environment)
+        > NEW.action_time;
+END;
+
+CREATE TRIGGER AliveBug_update_action_time
+BEFORE UPDATE OF action_time
+ON AliveBug
+FOR EACH ROW
+BEGIN
+  SELECT RAISE(ROLLBACK, 'The action_time is in the past')
+  WHERE (SELECT max(time)
+         FROM Environment)
+        > NEW.action_time;
+END;
+
+CREATE TRIGGER AliveBug_delete
+AFTER DELETE
+ON AliveBug
+FOR EACH ROW
+BEGIN
+  DELETE FROM Registers
+  WHERE id = OLD.registers_id;
+END;
+
+COMMIT;
